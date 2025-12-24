60a6985a91a4a29e0e7fa198df0673d138163d44
pinnagryph-sudo <aquafelispinnagryph@gmail.com>
2025-12-24 01:07:38 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Vaeloris Service Worker - Fixed for GitHub Pages
-const CACHE_NAME = 'vaeloris-v5';
+const CACHE_NAME = 'vaeloris-v6';
 
 // Install - cache everything needed for offline
 self.addEventListener('install', (event) => {
